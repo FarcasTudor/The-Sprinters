721cee4bda3ac606f5cb07d72f259f41b8373235
Tudor Farcas <tudor.farcas2@stud.ubbcluj.ro>
2024-03-10 17:23:29 +0200

    Initial commit

---
+++ the-sprinters-back/src/main/java/com/sprinters/dtos/RegistrationDto.java
@@ -0,0 +1,15 @@
+package com.sprinters.dtos;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegistrationDto {
+    private String username;
+    private String password;
+    private String userType;
+}
